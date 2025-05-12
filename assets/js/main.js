173f88bb1d621214963ac74c1c3da80875dc792d
mzelder <maciej.zelder@wp.pl>
2025-05-12 14:53:18 +0200

    mobile dropdown animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBtn = document.getElementById("mobile-menu-btn");
     const mobileDropdown = document.getElementById("mobile-dropdown");
+    
+    const servicesBtn = document.getElementById("services-btn");
+    const servicesDropdown = document.querySelector(".services-container");
 
     menuBtn.addEventListener("click", () => {
         const isOpen = menuBtn.classList.toggle("menu-open");
         mobileDropdown.classList.toggle("show", isOpen);
+    });
+
+    servicesBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        servicesDropdown.classList.toggle("menu-open");
     });
 });
