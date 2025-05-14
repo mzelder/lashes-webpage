873ac11385c1a0125f9838232c5bc9c3c659efa8
mzelder <maciej.zelder@wp.pl>
2025-05-14 10:27:06 +0200

    bg image added for first container

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const servicesBtn = document.getElementById("services-btn");
     const servicesDropdown = document.querySelector(".services-container");
 
+    const mainContainer = document.querySelector("#main-container");
     const footer = document.querySelector("footer");
 
     menuBtn.addEventListener("click", () => {
         const isOpen = menuBtn.classList.toggle("menu-open");
         mobileDropdown.classList.toggle("show", isOpen);
         footer.classList.toggle("hide", isOpen);
+        mainContainer.classList.toggle("hide", isOpen);
     });
 
     servicesBtn.addEventListener("click", (event) => {
