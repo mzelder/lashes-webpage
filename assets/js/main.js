0c3251a9986a106f53f909e4fc297d799daf9972
mzelder <maciej.zelder@wp.pl>
2025-05-15 15:29:54 +0200

    ADD: dekstop version of services section in index.html, CHG: Footer position to stick to the bottom correcly

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,36 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
         servicesDropdown.classList.toggle("menu-open");
     });
+
+    // services carousel
+    const carousel = document.getElementById('carousel-wrapper');
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    carousel.addEventListener('mousedown', (e) => {
+    isDown = true;
+    carousel.classList.add('dragging');
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+    });
+
+    carousel.addEventListener('mouseleave', () => {
+    isDown = false;
+    carousel.classList.remove('dragging');
+    });
+
+    carousel.addEventListener('mouseup', () => {
+    isDown = false;
+    carousel.classList.remove('dragging');
+    });
+
+    carousel.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - carousel.offsetLeft;
+        const walk = (x - startX);
+        carousel.scrollLeft = scrollLeft - walk;
+    });
 });
